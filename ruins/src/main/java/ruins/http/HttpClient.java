8aea798c7eacd441f06ca590f33a0fbdf2085992
jihaopeng <jihaopeng@gmail.com>
2016-09-12 18:49:34 +0800

    add logging with Retrofit 2.

--- ruins/src/main/java/ruins/http/HttpClient.java
+++ ruins/src/main/java/ruins/http/HttpClient.java
@@ -1,5 +1,7 @@
 package ruins.http;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -10,6 +12,10 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class HttpClient {
     private static HttpClient httpClient;
     private Retrofit retrofit;
+
+    HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+    OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
     public static HttpClient getInstance(String baseUrl){
         if (httpClient==null){
             synchronized (HttpClient.class){
@@ -21,8 +27,10 @@ public class HttpClient {
     }
 
     public HttpClient(String baseUrl){
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
         retrofit=new Retrofit.Builder()
                 .baseUrl(baseUrl)
+                .client(client)
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                 .build();
