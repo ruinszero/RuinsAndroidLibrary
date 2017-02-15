c8746a4bb9462f79de3036c90bfc0eb7c4918710
jihaopeng <jihaopeng@gmail.com>
2017-02-15 14:05:03 +0800

    设置超时时间。

--- ruins/src/main/java/ruins/http/HttpClient.java
+++ ruins/src/main/java/ruins/http/HttpClient.java
@@ -1,5 +1,7 @@
 package ruins.http;
 
+import java.util.concurrent.TimeUnit;
+
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
@@ -39,8 +41,9 @@ public class HttpClient {
             //设置Log
             builder.addInterceptor(interceptor);
         }
-
+        builder.connectTimeout(10, TimeUnit.SECONDS);   //设置超时时间
         OkHttpClient client = builder.build();
+
         retrofit=new Retrofit.Builder()
                 .baseUrl(baseUrl)
                 .client(client)
