3811dd3bafb94995461028ea2bac292ce4eef345
Ruins <jihaopeng@gmail.com>
2017-06-01 17:47:07 +0800

    修复rx升级导致的问题。

--- app/src/main/java/ruins/ruinsandroidlibrary/MainActivity.java
+++ app/src/main/java/ruins/ruinsandroidlibrary/MainActivity.java
@@ -7,16 +7,11 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.transition.Explode;
-import android.util.Log;
 import android.view.Window;
 import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import rx.Observable;
-import rx.Subscriber;
-import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
     @BindView(R.id.tv_navigationview)
@@ -39,68 +34,69 @@ public class MainActivity extends AppCompatActivity {
                     "content://media/internal/images/media"));
             startActivity(intent);
         });
-        tvTransition.setOnClickListener(v -> startActivity(new Intent(this,TransitionActivity.class), ActivityOptions.makeSceneTransitionAnimation(this).toBundle()));
+        tvTransition.setOnClickListener(v -> startActivity(new Intent(this, TransitionActivity.class), ActivityOptions.makeSceneTransitionAnimation(this).toBundle()));
 
 
-        test()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(integers1 -> {
-                    Log.e("Thread", Thread.currentThread().getName()+integers1);
-                });
-//        Observable.from(testThread()).subscribeOn(Schedulers.io()).flatMap(new Func1<Integer, Observable<String>>() {
-//            @Override
-//            public Observable<String> call(final Integer integer) {
-//                return Observable.create(new Observable.OnSubscribe<String>() {
-//                    @Override
-//                    public void call(Subscriber<? super String> subscriber) {
-//                        Log.e("my", "call: FlatMap " + Thread.currentThread().getName());
-//                        try {
-//                            Thread.sleep(200);
-//                            subscriber.onNext(integer + 100 + " FlatMap");
-//                            subscriber.onCompleted();
-//                        } catch (InterruptedException e) {
-//                            e.printStackTrace();
-//                            subscriber.onError(e);
-//                        }
-//                    }
-//                }).subscribeOn(Schedulers.newThread());
-//            }
-//        }).observeOn(AndroidSchedulers.mainThread())
-//                .subscribe(new Subscriber<String>() {
-//                    @Override
-//                    public void onCompleted() {
-//                        Log.e("my", "onCompleted: FlatMap");
-//                    }
+////        test()
+////                .subscribeOn(Schedulers.io())
+////                .observeOn(AndroidSchedulers.mainThread())
+////                .subscribe(integers1 -> {
+////                    Log.e("Thread", Thread.currentThread().getName()+integers1);
+////                });
+////        Observable.from(testThread()).subscribeOn(Schedulers.io()).flatMap(new Func1<Integer, Observable<String>>() {
+////            @Override
+////            public Observable<String> call(final Integer integer) {
+////                return Observable.create(new Observable.OnSubscribe<String>() {
+////                    @Override
+////                    public void call(Subscriber<? super String> subscriber) {
+////                        Log.e("my", "call: FlatMap " + Thread.currentThread().getName());
+////                        try {
+////                            Thread.sleep(200);
+////                            subscriber.onNext(integer + 100 + " FlatMap");
+////                            subscriber.onCompleted();
+////                        } catch (InterruptedException e) {
+////                            e.printStackTrace();
+////                            subscriber.onError(e);
+////                        }
+////                    }
+////                }).subscribeOn(Schedulers.newThread());
+////            }
+////        }).observeOn(AndroidSchedulers.mainThread())
+////                .subscribe(new Subscriber<String>() {
+////                    @Override
+////                    public void onCompleted() {
+////                        Log.e("my", "onCompleted: FlatMap");
+////                    }
+////
+////                    @Override
+////                    public void onError(Throwable e) {
+////                        Log.e("my", "onError: FlatMap");
+////                    }
+////
+////                    @Override
+////                    public void onNext(String s) {
+////                        Log.e("my", "onNext: FlatMap " + s);
+////                    }
+////                });
+//    }
 //
-//                    @Override
-//                    public void onError(Throwable e) {
-//                        Log.e("my", "onError: FlatMap");
-//                    }
+//    public Integer[] testThread() {
+//        Log.e("my++", Thread.currentThread().getName());
+//        return new Integer[]{1,2,3};
+//    }
 //
-//                    @Override
-//                    public void onNext(String s) {
-//                        Log.e("my", "onNext: FlatMap " + s);
-//                    }
-//                });
-    }
-
-    public Integer[] testThread() {
-        Log.e("my++", Thread.currentThread().getName());
-        return new Integer[]{1,2,3};
-    }
-
-    public Observable<Integer> test() {
-        return Observable.create(new Observable.OnSubscribe<Integer>() {
-            @Override
-            public void call(Subscriber<? super Integer> subscriber) {
-                Log.e("my", Thread.currentThread().getName());
-                subscriber.onNext(1);
-                subscriber.onNext(2);
-                subscriber.onNext(3);
-                subscriber.onCompleted();
-            }
-        }).subscribeOn(Schedulers.io());
+////    public Observable<Integer> test() {
+////        return Observable.create(new Observable.OnSubscribe<Integer>() {
+////            @Override
+////            public void call(Subscriber<? super Integer> subscriber) {
+////                Log.e("my", Thread.currentThread().getName());
+////                subscriber.onNext(1);
+////                subscriber.onNext(2);
+////                subscriber.onNext(3);
+////                subscriber.onCompleted();
+////            }
+////        }).subscribeOn(Schedulers.io());
+////    }
     }
 
 }
--- ruins/src/main/java/ruins/http/HttpClient.java
+++ ruins/src/main/java/ruins/http/HttpClient.java
@@ -5,7 +5,7 @@ import java.util.concurrent.TimeUnit;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
@@ -49,7 +49,7 @@ public class HttpClient {
                 .baseUrl(baseUrl)
                 .client(client)
                 .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build();
     }
 
